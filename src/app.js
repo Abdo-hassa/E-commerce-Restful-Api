b9c88844d26c75f673e9e9d90073cc59de99b04d
Abdo-hassa <abdo45010@gmail.com>
2022-03-02 14:24:58 +0200

    setup cors

--- src/app.js
+++ src/app.js
@@ -7,22 +7,15 @@ const CartRoute = require('./routes/cart');
 const OrderRoute = require('./routes/order');
 const Stripe = require('./routes/stripe');
 
-const cors = require('cors')
+const cors = require('cors');
+app.use(cors());
 
 const { errorHandler, notFound } = require('./middleware/errorMiddlewares');
 app.use(express.json());
 
-app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-	next();
-});
-
 app.get('/', (req, res) => {
 	res.send('api is running');
 });
-app.use(cors())
 app.use('/api/auth', AuthRoute);
 app.use('/api/user', UserRoute);
 app.use('/api/product', ProductRoute);
--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -9,7 +9,6 @@ exports.generateCriteriaObject = obj => {
 		if (obj[prop] === 'any' || obj[prop] === '' || obj[prop].length === 0) {
 			delete obj[prop];
 		}
-		// lw el prop = name w el name msh empty 7ott el query bta3t el search f el obj data
 		if (prop === 'title' && obj[prop] !== '' && obj[prop] !== undefined) {
 			data['$text'] = { $search: `${obj[prop]}` };
 		} else if (prop === 'categories' && obj[prop]?.length > 0) {
